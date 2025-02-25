ebc2c3bc370bce64dc374cbcea22a58d9b3f438f
Dino <86893313+dinorawraw@users.noreply.github.com>
2025-02-25 17:57:36 -0300

    Delete src/main.js

--- src/main.js
+++
@@ -1,7 +0,0 @@
-// Main application entry point
-import { SessionManager } from './sessionManager';
-import { PageGuard } from './pageGuard';
-
-// Clear any existing session data and initialize session management
-SessionManager.init();
-PageGuard.init();
